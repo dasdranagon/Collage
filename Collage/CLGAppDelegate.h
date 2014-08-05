0ce633c2a20ee5117ed4c5443d7e524667fd3150
Denis Skokov <denis.skokov@together.com>
2014-08-05 16:14:56 +0400

    Initial Commit

---
+++ Collage/CLGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CLGAppDelegate.h
+//  Collage
+//
+//  Created by Denis Skokov on 05.08.14.
+//  Copyright (c) 2014 Denis Skokov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CLGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Collage/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Collage
+//
+//  Created by Denis Skokov on 05.08.14.
+//  Copyright (c) 2014 Denis Skokov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CLGAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CLGAppDelegate class]));
+    }
+}
